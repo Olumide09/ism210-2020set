29519460609bed4fdefc8549d289de452e36eaad
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-04-05 17:47:22 +0100

    Update App.js

--- react-crash-course/my-firstapp/src/App.js
+++ react-crash-course/my-firstapp/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import HelloWorld from "./components/HelloWorld";
+import HelloWorld2 from "./components/HelloWorld2";
 const App = () => {
   return (
     <div>
       <HelloWorld />
+      <HelloWorld2 name="Olumide"/>
     </div>
   );
 };
