73fd84c07412bf6035ddf7456224032c4074f111
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-04-05 17:29:51 +0100

    Update App.js

--- react-crash-course/my-firstapp/src/App.js
+++ react-crash-course/my-firstapp/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import HelloWorld from './components/HelloWorld';
+import React from "react";
+import HelloWorld from "./components/HelloWorld";
 const App = () => {
   return (
     <div>
       <HelloWorld />
     </div>
   );
-}
+};
 export default App;
