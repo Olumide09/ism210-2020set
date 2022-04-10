26eb754004226b5f0a4cc25a802b307a3200e7c1
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-04-10 17:24:34 +0100

    helloworld4 update

--- react-crash-course/my-first-app-with-ts/src/App.tsx
+++ react-crash-course/my-first-app-with-ts/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import HelloWorld from './components/HelloWorld';
 import HelloWorld2 from './components/HelloWorld2';
 import HelloWorld3 from './components/HelloWorld3';
+import HelloWorld4 from './components/HelloWorld4';
 const App: React.FC = () => {
   return (
     <div>
       <HelloWorld />
       <HelloWorld2 name="Olumide"/>
       <HelloWorld3 name="Demilade"/>
+      <HelloWorld4 />
     </div>
   );
 }
