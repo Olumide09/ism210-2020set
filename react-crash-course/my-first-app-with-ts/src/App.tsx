a8e85373973c566f4cf586b7bc31cc4fe9f9a5fc
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-04-10 15:57:26 +0100

    Update App.tsx

--- react-crash-course/my-first-app-with-ts/src/App.tsx
+++ react-crash-course/my-first-app-with-ts/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import HelloWorld from './components/HelloWorld';
+import HelloWorld2 from './components/HelloWorld2';
 const App: React.FC = () => {
   return (
     <div>
