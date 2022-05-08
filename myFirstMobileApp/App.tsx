d14eeef9fd64336b9a8128223d50677d3a79fc58
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-05-08 11:47:53 +0100

    Update App.tsx

--- myFirstMobileApp/App.tsx
+++ myFirstMobileApp/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Initial Code Adjusted to ES6+ and Typescript by Me!</Text>
       <StatusBar style="auto" />
     </View>
   );
