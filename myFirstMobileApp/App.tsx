aa0aef45efb5d05cead9adef1888f39f6b20cac9
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-05-08 22:44:07 +0100

    Update

--- myFirstMobileApp/App.tsx
+++ myFirstMobileApp/App.tsx
@@ -3,11 +3,18 @@ import { View, StyleSheet } from 'react-native';
 import Component1 from './src/components/Component1';
 import Component2 from './src/components/Component2';
 import Component3 from './src/components/Component3';
+//Let's create a simple separator component that will only be used here in App.tsx
+const Separator: React.FC = () => {
+  return <View style={styles.separator}></View>
+}
 const App: React.FC = () => {
+
   return (
     <View style={styles.container}>
       <Component1 />
+      <Separator />
       <Component2 />
+      <Separator />
       <Component3 />
     </View>
   );
@@ -15,9 +22,14 @@ const App: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'cyan',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  separator: {
+    backgroundColor: '#eee',
+    height: 3,
+    width: '100%'
   }
 });
 export default App;
