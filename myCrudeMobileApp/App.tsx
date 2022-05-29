e6619b8916abb81bb08e75492ca3b3b2ae2d53c1
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-05-29 15:56:29 +0100

    created entities and updates

--- myCrudeMobileApp/App.tsx
+++ myCrudeMobileApp/App.tsx
@@ -1,6 +1,13 @@
 import 'reflect-metadata';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
+import { TransactionEntry } from './src/entities/transaction-entry.entity';
+import { Connection } from 'typeorm/connection/Connection';
+import { createConnection } from 'typeorm';
+
+const [defaultConnection, setConnection] = useState<Connection | null>(null);
+const [transactionEntries, setTransactionEntries] = useState<TransactionEntry[]>([]);
+
 const App: React.FC = () => {
 
   return (
@@ -18,4 +25,26 @@ const styles = StyleSheet.create({
   },
   title: { fontSize: 16, color: 'black' },
 });
+
+const setupConnection = useCallback(async () => {
+  try {
+    const connection = await createConnection({
+      /* Use below if not using expo
+      type: 'react-native',
+      database: 'transaction_entries.db',
+      location: 'default',
+      */
+      type: 'expo',
+      database: 'transaction_entries.db',
+      driver: require('expo-sqlite'),
+      //logging: ['error', 'query', 'schema'],
+      synchronize: true,
+      entities: [TransactionEntry],
+    });
+    setConnection(connection);
+    getTransactionEntries(setTransactionEntries);
+  } catch (error) {
+    console.log(error);
+  }
+}, []);
 export default App;
