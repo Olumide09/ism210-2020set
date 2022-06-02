5110631b9286b837fd5d7835c7a33f5b04400cdb
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-06-02 21:11:41 +0100

    done

--- myCrudMobileApp/src/interfaces/transactionentry.interfaces.ts
+++
@@ -1,9 +0,0 @@
-interface ITransactionEntry{
-    id?: number;
-    txnDay?: number;
-    txnMonth?: number;
-    txnYear?: number;
-    description: string;
-    amount: number;
-    expense?: boolean
-   }
